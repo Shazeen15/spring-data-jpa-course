428551aea90c659e5a6c80c1d42337122d06e3e2
Shazeen Fabius <shazzy.sterlin@gmail.com>
2022-05-14 17:02:26 -0700

    learned to use Sort

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -5,6 +5,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.Sort;
 
 @SpringBootApplication
 public class Application {
@@ -17,26 +18,44 @@ public class Application {
     @Bean
     CommandLineRunner commandLineRunner(StudentRepository studentRepository) {
         return args -> {
-            Faker faker = new Faker();
-            for (int i = 0; i <= 20; i++) {
-                String firstName = faker.name()
-                    .firstName();
-                String lastName = faker.name()
-                    .lastName();
-                String email = String.format("%s.%s@email.com",
-                    firstName,
-                    lastName);
+            generateRandomStudent(studentRepository);
 
-                Student student = new Student(firstName,
-                    lastName,
-                    email,
-                    faker.number()
-                        .numberBetween(0,
-                            100));
 
-                studentRepository.save(student);
-            }
         };
+    }
+
+    private void sorting(StudentRepository studentRepository) {
+        Sort sortedByFirstName = Sort.by("firstName")
+            .ascending()
+            .and(Sort.by("age")
+                .descending());
+
+        studentRepository.findAll(sortedByFirstName)
+            .forEach(student -> {
+                System.out.println(student.getFirstName() + " -- " + student.getAge());
+            });
+    }
+
+    private void generateRandomStudent(StudentRepository studentRepository) {
+        Faker faker = new Faker();
+        for (int i = 0; i <= 1000; i++) {
+            String firstName = faker.name()
+                .firstName();
+            String lastName = faker.name()
+                .lastName();
+            String email = String.format("%s.%s@email.com",
+                firstName,
+                lastName);
+
+            Student student = new Student(firstName,
+                lastName,
+                email,
+                faker.number()
+                    .numberBetween(0,
+                        100));
+
+            studentRepository.save(student);
+        }
     }
 
 }
